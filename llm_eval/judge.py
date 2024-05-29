e9d189a2a3db1fced5c92b8257818b0eec6ca38a
Jonathan <eisen@discourse.ai>
2024-05-29 03:19:49 -0700

    extract_output_by_name fix

--- llm_eval/judge.py
+++ llm_eval/judge.py
@@ -13,14 +13,13 @@ class LLMJudge:
     def extract_output_by_name(self, name, value):
         if isinstance(value, float) and np.isnan(value):
             return ''
-        #match1 = re.search(rf'{name}\s?:\s*.*?(\w+.*)', value, re.DOTALL)
-        #match1 = re.search(r'\{\s*"(.+?)"\s*:\s*"(.+?)"\s*\}')
-        match1 = re.search(r'\{\s*"' + name + r'"\s*:\s*"(.+?)"\s*\}')
-        if match1:
-            return match1.group(1).strip()
+        pattern = re.compile(r'\{\s*["\'](.+?)["\']\s*:\s*["\'](.+?)["\']\s*\}')
+        matches = re.findall(pattern, value)
+        if matches:
+            last_match = matches[-1]
+            return last_match[1].strip()
         else:
             return value
-        return ''
 
     def evaluate(self):
         client = OpenAI(
